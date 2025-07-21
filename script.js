b907f6465114110eac05547669627ca88ccc5528
cadmielannylove <joaocadmiel50@gmail.com>
2025-07-21 19:12:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-
-(function() {
-  emailjs.init("fvIS17Nphrm-Y2v85"); // sua public key
+(function () {
+  emailjs.init("fvIS17Nphrm-Y2v85"); // sua public key real
 })();
 
-document.getElementById("carinho-form").addEventListener("submit", function(e) {
+document.getElementById("carinho-form").addEventListener("submit", function (e) {
   e.preventDefault();
-  const email = document.getElementById("user_email").value;
+
+  const userEmail = this.user_email.value;
 
   emailjs.send("service_hu38i0n", "template_8u1eo1o", {
-    user_email: email
-  }).then(() => {
+    user_email: userEmail
+  })
+  .then(function () {
     document.getElementById("mensagem-sucesso").style.display = "block";
+  })
+  .catch(function (error) {
+    console.error("Erro ao enviar:", error);
+    alert("Erro ao enviar o carinho 😢 Verifique se está tudo certinho.");
   });
 });
